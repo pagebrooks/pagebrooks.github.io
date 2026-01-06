fce9afce78f351d40e7717830b768fd4ae3a3555
Page Brooks <pagebrooks@gmail.com>
2026-01-05 16:32:14 -0800

    Add type effect

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,23 @@
 console.log("👋 If you're checking my console, you either want to hire me or debug me. Either way, let's talk.");
 
-var menu = document.getElementById("menu");
-var initialMenuOffset = menu.offsetTop;
-window.onscroll = function() {
-   if(window.pageYOffset > initialMenuOffset) {
-	   menu.classList.add("sticky");
-   } else {
-	   menu.classList.remove("sticky");
-   }
-};
+document.addEventListener("DOMContentLoaded", function() {
+   var menu = document.getElementById("menu");
+   var initialMenuOffset = menu.offsetTop;
+   window.onscroll = function() {
+      if(window.pageYOffset > initialMenuOffset) {
+         menu.classList.add("sticky");
+      } else {
+         menu.classList.remove("sticky");
+      }
+   };
+});
 
 // Typing effect for tagline
-(function() {
+document.addEventListener("DOMContentLoaded", function() {
    var tagline = document.getElementById("tagline");
    var text = "Computers love to read what I write.";
    var index = 0;
-   var typingSpeed = 50;
+   var typingSpeed = 20;
 
    function type() {
       if (index < text.length) {
@@ -26,4 +28,4 @@ window.onscroll = function() {
    }
 
    type();
-})();
+});
